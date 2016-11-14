b2f351cdd1cd1cedf811df23d29a3dcd5c855452
Earth Engine <earthengine@gmail.com>
2016-11-14 17:41:19 +1100

    Add project files.

---
+++ CSSocks/Program.cs
@@ -0,0 +1,13 @@
+﻿using Socks5Lib;
+
+namespace CSSocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svr = new Socks5Server();
+            svr.Start();
+        }
+    }
+}
---
+++ Socks5Lib/Socks5Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Socks5Lib
+{
+    public class Socks5Server
+    {
+        public void Start()
+        {
+            var sli = new 
+        }
+    }
+}
